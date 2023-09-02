e4dc0bbfe4902a016fafbf1b753f4e4ed7dc1dfb
atique-ahmad <atique.ahmad@formsoft-inc.com>
2023-09-02 11:27:38 +0500

    scrum app created successfully

--- scrum/app/models.py
+++ scrum/app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=50)
+    project_description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set on creation
+    
+    def __str__(self) -> str:
+        return self.project_name
+
+class Ticket(models.Model):
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Done', 'Done'),
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    ticket_title = models.CharField(max_length=50)
+    ticket_description = models.TextField()
+    ticket_status = models.CharField(choices=STATUS_CHOICES, max_length=50)
+    
+    def __str__(self) -> str:
+        return self.ticket_title
